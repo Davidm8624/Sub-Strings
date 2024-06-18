1597a7dd50331554168535ff43840f134620ab87
davidm8624 <davidm8624@gmail.com>
2024-06-17 17:30:44 -0700

    misc

--- sub.rb
+++ sub.rb
@@ -1,16 +1,16 @@
-dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it","i","low","own","part","partner","sit"]
+dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
 
-#next time just manke the array with %w and type normally because it will automaticly consider arrays with teh %w to be an array of strings automaticly split my spaces
+# next time just manke the array with %w and type normally because it will automaticly consider arrays with teh %w to be an array of strings automaticly split my spaces
 
 def substrings(text, dictionary)
   text = text.downcase.split
 
   substrings_words = []
 
-  #this parts goes through each of the words provided by the user *text
+  # this parts goes through each of the words provided by the user *text
   text.each do |word_text|
-    #takes the words in dictionary that are substrings of word_text
-    #what .select does is it returns the values that are evaluated to be true, for example
+# takes the words in dictionary that are substrings of word_text
+# what .select does is it returns the values that are evaluated to be true, for example
 =begin
 
 even_numbers = []
@@ -29,16 +29,16 @@ or you could use select:
 
 =end
 
-substrings_words += dictionary.select { |word| word_text.include?(word) }
-end
+    substrings_words += dictionary.select { |word| word_text.include?(word) }
+  end
 
-#makes the hash
-substring_hash = Hash.new(0)
-substrings_words.each do |word|
-  substring_hash[word] += 1
-end
+  # makes the hash
+  substring_hash = Hash.new(0)
+  substrings_words.each do |word|
+    substring_hash[word] += 1
+  end
 
-puts("#{substring_hash}")
+  puts("#{substring_hash}")
 end
 
 substrings("Howdy partner, sit down! How's it going?", dictionary)
